df44fd480bfc5b502ef3ad73581093e8f501381b
gsagariw <gsagariw@gmail.com>
2018-10-09 08:57:43 +0545

    CRUD View Files Upload

--- etl/urls.py
+++ etl/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.CsvDataListView.as_view(), name='csvdata_list'),
     path('create/', views.CsvDataCreateView.as_view(), name='csvdata_create'),
     path('detail/<int:pk>', views.CsvDataDetailView.as_view(), name='etl_csvdata_detail'),
-    path('update/<int:pk>', views.CsvDataUpdateView.as_view(), name='etl_csvdata_update')
+    path('update/<int:pk>', views.CsvDataUpdateView.as_view(), name='etl_csvdata_update'),
+    path('delete/<int:pk>', views.CsvDataDeleteView.as_view(), name='etl_csvdata_delete'),
+    path('upload/', views.upload_process, name='upload_csv_file')
 ]
--- etl/views.py
+++ etl/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+import csv
+
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from .models import CsvData
 from .forms import CsvDataForm
@@ -27,3 +30,28 @@ class CsvDataDetailView(DetailView):
 class CsvDataUpdateView(UpdateView):
     model = CsvData
     form_class = CsvDataForm
+
+class CsvDataDeleteView(DeleteView):
+    model = CsvData
+    success_url = reverse_lazy('csvdata_list')
+
+
+def upload_process(request):
+
+    if request.method == 'POST':
+        file = request.FILES['file_input_name']
+        file_data = file.read().decode('utf-8').splitlines()
+        reader = csv.DictReader(file_data)
+        for each in reader:
+            name = each.get('Name')
+            ip_address = each.get('IPV4')
+            mac_address = each.get('MAC_ADDRESS')
+            CsvData.objects.create(
+                name=name,
+                ipv4 =ip_address,
+                mac_address =mac_address
+            )
+
+        return redirect('csvdata_list')
+
+    return render(request, template_name='etl/file_upload.html', context={})
